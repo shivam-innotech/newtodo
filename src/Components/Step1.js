70203b206c2cbe3122e005a596daab6cb2a341de
shivam-innotech <Shivam@innotechteam.in>
2022-08-26 11:35:17 +0530

    step1

---
+++ src/Components/Step1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function step1() {
+return (
+<div>
+    <h1>Todo App</h1>
+    <div className="box">
+        <input type="checkbox" className="ckbox" id="" />
+        <lable>A</lable>
+        <p className='badge bg-secondary bag'>Compleate</p>
+    </div><br />
+</div>
+)
+}
